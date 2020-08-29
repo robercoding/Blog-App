f30afbdda3b83bbf08ac89d7477b5b5c1e6fcf7b
Roberto Fuentes <robercoding@gmail.com>
2020-08-29 13:48:04 +0200

    ProfileEdit: Added layout

---
+++ app/src/main/java/com/rober/blogapp/ui/main/profile/profileedit/ProfileEditState.kt
@@ -0,0 +1,6 @@
+package com.rober.blogapp.ui.main.profile.profileedit
+
+sealed class ProfileEditState {
+    object LoadingUser: ProfileEditState()
+
+}
---
+++ app/src/main/java/com/rober/blogapp/ui/main/profile/profileedit/ProfileEditViewModel.kt
@@ -0,0 +1,8 @@
+package com.rober.blogapp.ui.main.profile.profileedit
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class ProfileEditViewModel @ViewModelInject constructor(): ViewModel() {
+
+}
