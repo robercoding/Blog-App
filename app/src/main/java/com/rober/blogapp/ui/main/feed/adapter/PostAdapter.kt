d4d6aba842cba174ebd863e1c90b5100bc63958e
Roberto Fuentes <robercoding@gmail.com>
2020-09-22 08:48:18 +0200

    Added how much time passed from that post in PostAdapterFeed, now you don't have to access to the post itself to see the time. Changed scrollbar size

--- app/src/main/java/com/rober/blogapp/ui/main/feed/adapter/PostAdapter.kt
+++ app/src/main/java/com/rober/blogapp/ui/main/feed/adapter/PostAdapter.kt
@@ -15,9 +15,16 @@ import com.rober.blogapp.R
 import com.rober.blogapp.entity.Post
 import com.rober.blogapp.entity.User
 import com.rober.blogapp.util.RecyclerViewActionInterface
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
+import org.threeten.bp.temporal.ChronoUnit
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.math.abs
 
 
-class PostAdapter (val itemView: View, val viewHolder: Int, val recyclerViewActionInterface: RecyclerViewActionInterface) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+class PostAdapter(val itemView: View, val viewHolder: Int, val recyclerViewActionInterface: RecyclerViewActionInterface) :
+    RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         //return PostViewHolder()
@@ -25,7 +32,7 @@ class PostAdapter (val itemView: View, val viewHolder: Int, val recyclerViewActi
         return PostViewHolder(view, recyclerViewActionInterface, differUser)
     }
 
-    private val differPostCallback = object: DiffUtil.ItemCallback<Post>(){
+    private val differPostCallback = object : DiffUtil.ItemCallback<Post>() {
         override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
             return oldItem.postId == newItem.postId
         }
@@ -39,7 +46,7 @@ class PostAdapter (val itemView: View, val viewHolder: Int, val recyclerViewActi
         }
     }
 
-    private val differUserCallback = object: DiffUtil.ItemCallback<User>(){
+    private val differUserCallback = object : DiffUtil.ItemCallback<User>() {
         override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
             return oldItem.user_id == newItem.user_id
         }
@@ -66,44 +73,50 @@ class PostAdapter (val itemView: View, val viewHolder: Int, val recyclerViewActi
 //        }
     }
 
-    fun setPosts(newListPost: MutableList<Post>){
+    fun setPosts(newListPost: MutableList<Post>) {
         differPost.submitList(newListPost)
         notifyDataSetChanged()
     }
 
-    fun setUsers(newListUsers: MutableList<User>){
+    fun setUsers(newListUsers: MutableList<User>) {
         differUser.submitList(newListUsers)
         notifyDataSetChanged()
     }
 
-    class PostViewHolder(itemView: View, val recyclerViewActionInterface: RecyclerViewActionInterface, val differUser:AsyncListDiffer<User>): RecyclerView.ViewHolder(itemView) {
+    class PostViewHolder(
+        itemView: View,
+        val recyclerViewActionInterface: RecyclerViewActionInterface,
+        val differUser: AsyncListDiffer<User>
+    ) : RecyclerView.ViewHolder(itemView) {
 
-        var uid_image : ImageView? = null
-        var uid_name : TextView? = null
+        var uid_image: ImageView? = null
+        var uid_name: TextView? = null
         var title: TextView? = null
         var text: TextView? = null
+        var time: TextView? = null
 
         var container_post: ConstraintLayout? = null
         var container_no_more_posts: ConstraintLayout? = null
 
-        init{
+        init {
             uid_image = itemView.findViewById(R.id.uid_image)
             uid_name = itemView.findViewById(R.id.uid_name)
             title = itemView.findViewById(R.id.title)
             text = itemView.findViewById(R.id.text)
+            time = itemView.findViewById(R.id.time)
             container_post = itemView.findViewById(R.id.feed_viewholder_container_post)
             container_no_more_posts = itemView.findViewById(R.id.feed_viewholder_container_no_more_posts)
         }
 
-        fun bind(post: Post){
+        fun bind(post: Post) {
             Log.i("PostAdapter", post.userCreatorId)
             Log.i("PostAdapter", "${differUser.currentList}")
-            if(post.created_at == 0.toLong()){
+            if (post.created_at == 0.toLong()) {
                 container_no_more_posts?.visibility = View.VISIBLE
                 container_post?.visibility = View.GONE
-            }else{
-                val user = differUser.currentList.find { user-> user.user_id == post.userCreatorId }
-                user?.let {tempUser ->
+            } else {
+                val user = differUser.currentList.find { user -> user.user_id == post.userCreatorId }
+                user?.let { tempUser ->
                     uid_image?.let {
                         Glide.with(itemView)
                             .load(tempUser.profileImageUrl)
@@ -111,24 +124,63 @@ class PostAdapter (val itemView: View, val viewHolder: Int, val recyclerViewActi
                     }
 
                     uid_name?.text = "@${tempUser.username}"
+                    val diffTime = getDifferenceTime(post.created_at)
+                    time?.text = diffTime
                     setupClickListeners()
-                }?: kotlin.run {
+                } ?: kotlin.run {
                     uid_name?.text = "@Unknown user"
                 }
                 title?.text = post.title
                 text?.text = post.text
                 container_no_more_posts?.visibility = View.GONE
                 container_post?.visibility = View.VISIBLE
+                time?.visibility = View.VISIBLE
             }
         }
 
-        private fun setupClickListeners(){
-            title?.setOnClickListener {recyclerViewActionInterface.clickListenerOnPost(adapterPosition)}
-            text?.setOnClickListener {recyclerViewActionInterface.clickListenerOnPost(adapterPosition)}
+        private fun setupClickListeners() {
+            title?.setOnClickListener { recyclerViewActionInterface.clickListenerOnPost(adapterPosition) }
+            text?.setOnClickListener { recyclerViewActionInterface.clickListenerOnPost(adapterPosition) }
             container_post?.setOnClickListener { recyclerViewActionInterface.clickListenerOnPost(adapterPosition) }
 
             uid_name?.setOnClickListener { recyclerViewActionInterface.clickListenerOnUser(adapterPosition) }
-            uid_image?.setOnClickListener { recyclerViewActionInterface.clickListenerOnUser(adapterPosition)}
+            uid_image?.setOnClickListener { recyclerViewActionInterface.clickListenerOnUser(adapterPosition) }
+        }
+
+        private fun getDifferenceTime(postDate: Long): String {
+            val instant = Instant.ofEpochSecond(postDate)
+            val instantPostMillis = instant.toEpochMilli()
+
+            val instantNow = Instant.now().toEpochMilli()
+            val diff = abs(instantNow - instantPostMillis)
+            val diffDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS).toInt()
+
+            when (diffDays) {
+                in 1..364 -> {
+                    return "${diffDays}d"
+                }
+
+                0 -> {
+                    val diffHours = TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS).toInt()
+                    if (diffHours > 0) {
+                        return "${diffHours}h"
+                    }
+                    val diffMinutes = TimeUnit.MINUTES.convert(diff, TimeUnit.MILLISECONDS).toInt()
+                    if (diffMinutes > 0) {
+                        return "${diffMinutes}m"
+                    }
+                    val diffSeconds = TimeUnit.SECONDS.convert(diff, TimeUnit.MILLISECONDS).toInt()
+                    return "${diffSeconds}s "
+                }
+
+                else -> {
+                    val diffDays = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS).toInt()
+                    return "${diffDays / 365}y"
+                }
+
+            }
         }
     }
+
+
 }
